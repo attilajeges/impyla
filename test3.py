6bf393c79f4b6328c4632a3ef9665fbfb2e63889
Attila Jeges <attilaj@cloudera.com>
2020-08-25 09:56:38 +0000

    Implement GSSAPI authentication over http transport.
    The implementation adds a new dependency on 'kerberos' python module,
    which is a pip-installed module distributed under Apache License Version 2.
    
    When using impyla with GSSAPI over http, it is assumed that the client has a
    preexisting kinit-cached Kerberos ticket that impyla can pass to the server
    automatically without the user needing to reenter their password.
    
    Examples:
    
    1. Hive
    conn = connect('hiveserver2.example.com', 10001,
      auth_mechanism="GSSAPI",
      kerberos_service_name='hive',
      use_http_transport=True,
      http_path='cliservice',
      auth_cookie_name='hive.server2.auth')
    cursor = conn.cursor()
    cursor.execute("show databases")
    res = cursor.fetchall()
    
    2. Impala
    conn = connect('impala-coordinator.exmple.com', 28000,
      auth_mechanism="GSSAPI",
      kerberos_service_name='impala',
      use_http_transport=True,
      http_path='cliservice',
      auth_cookie_name='impala.auth')
    cursor = conn.cursor()
    cursor.execute("show databases")
    res = cursor.fetchall()

---
+++ test3.py
@@ -0,0 +1,56 @@
+#!/opt/rh/rh-python36/root/usr/bin/python3
+
+import os
+import time
+from impala.dbapi import connect
+from impala.util import as_pandas
+
+import logging
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+root_logger.addHandler(logging.StreamHandler())
+
+# host = 'attilaj-1.attilaj.root.hwx.site'
+# test_hive_port = 10000
+# conn = connect(host, test_hive_port,
+#   auth_mechanism="GSSAPI",
+#   kerberos_service_name='hive',
+#   password='Password@123')
+
+host = 'attilaj-1.attilaj.root.hwx.site'
+test_hive_port = 10001
+conn = connect(host, test_hive_port,
+  auth_mechanism="GSSAPI",
+  kerberos_service_name='hive',
+  use_http_transport=True,
+  http_path='cliservice',
+  auth_cookie_name='hive.server2.auth')
+
+cursor = conn.cursor()
+
+cursor.execute("show databases")
+print('show databases 1: %s' %  cursor.fetchall())
+time.sleep(2)
+
+cursor.execute("show tables")
+print('show tables 2: %s' %  cursor.fetchall())
+time.sleep(2)
+
+cursor.execute("show databases")
+print('show databases 3: %s' %  cursor.fetchall())
+time.sleep(2)
+
+cursor.execute("drop table if exists tbl")
+time.sleep(2)
+cursor.execute("create table tbl (id integer, s string)")
+time.sleep(2)
+
+insert_parts = ["insert into tbl(id, s) values (133, 'xxx')"]
+for i in range(0, 1000):
+  insert_parts.append("(%s, 'bbb')" % i)
+stmt = ', '.join(insert_parts)
+cursor.execute(stmt)
+time.sleep(2)
+
+cursor.execute("select id, s from tbl")
+print('select id, s rom tbl: %s' %  cursor.fetchall())
