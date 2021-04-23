04c91ae4c7c98f7f7841747a3bbb92efd1c448e9
Attila Jeges <attilaj@cloudera.com>
2021-04-23 17:35:18 +0200

    TESTING: Add test scripts for impyla + pure-sasl
    Test with python2 / python3 and binary / http transport the following:
    - impala nosasl, gssapi
    - hive plain, gssapi
    
    Start tests with ubuntu and centos:
    ./test-ubuntu.sh
    ./test-centos.sh

---
+++ io/test_scripts/test-hive-py2-plain.py
@@ -0,0 +1,34 @@
+from impala.dbapi import connect
+
+def binary():
+  conn = connect('attilaj-1.attilaj.root.hwx.site',
+    10000,
+    auth_mechanism="PLAIN",
+  )
+
+  cursor = conn.cursor()
+  cursor.execute("select * from web_logs")
+  res = cursor.fetchall()
+  print len(res)
+
+  cursor.close()
+  conn.close()
+
+def hs2_http():
+  conn = connect('attilaj-1.attilaj.root.hwx.site',
+    10001,
+    auth_mechanism="PLAIN",
+    use_http_transport=True,
+    http_path='cliservice',
+  )
+
+  cursor = conn.cursor()
+  cursor.execute("select * from web_logs")
+  res = cursor.fetchall()
+  print len(res)
+
+  cursor.close()
+  conn.close()
+
+hs2_http()
+binary()
---
+++ io/test_scripts/test-impala-py3-ldap.py
@@ -0,0 +1,22 @@
+from impala.dbapi import connect
+
+def hs2_http():
+  conn = connect('coordinator-denys-test1-impala.apps.dev-02.kcloud.cloudera.com',
+    443,
+    auth_mechanism="LDAP",
+    user='cm-admin',
+    password='Test123',
+    use_ssl=True,
+    use_http_transport=True,
+    http_path='cliservice',
+  )
+  
+  cursor = conn.cursor()
+  cursor.execute("show databases;")
+  res = cursor.fetchall()
+  print(len(res))
+  
+  cursor.close()
+  conn.close()
+
+hs2_http()
