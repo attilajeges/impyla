1e28c4c678510e4354c1f895cb1bb105c296ad13
Attila Jeges <attilaj@cloudera.com>
2021-05-21 14:41:30 +0200

    TESTING: Add test scripts

---
+++ io/test_scripts/test-impala-py2-nosasl.py
@@ -0,0 +1,36 @@
+from impala.dbapi import connect
+  
+# binary
+def binary():
+  conn = connect('attilaj-1.attilaj.root.hwx.site',
+    21050,
+    auth_mechanism="NOSASL",
+  )
+
+  cursor = conn.cursor()
+  cursor.execute("select * from web_logs")
+  res = cursor.fetchall()
+  print len(res)
+
+  cursor.close()
+  conn.close()
+
+# hs2-hhtp
+def hs2_http():
+  conn = connect('attilaj-1.attilaj.root.hwx.site',
+    28000,
+    auth_mechanism="NOSASL",
+    use_http_transport=True,
+    http_path='cliservice',
+  )
+
+  cursor = conn.cursor()
+  cursor.execute("select * from web_logs")
+  res = cursor.fetchall()
+  print len(res)
+
+  cursor.close()
+  conn.close()
+
+binary()
+hs2_http()
---
+++ io/test_scripts/test-impala-py3-gssapi.py
@@ -0,0 +1,38 @@
+from impala.dbapi import connect
+
+def binary():
+  conn = connect('betazoid-2.betazoid.root.hwx.site',
+    21050,
+    auth_mechanism="GSSAPI",
+    kerberos_service_name='impala',
+    use_ssl=True
+  )
+  
+  cursor = conn.cursor()
+  cursor.execute("select * from web_logs")
+  res = cursor.fetchall()
+  print(len(res))
+  
+  cursor.close()
+  conn.close()
+
+def hs2_http():
+  conn = connect('betazoid-2.betazoid.root.hwx.site',
+    28000,
+    auth_mechanism="GSSAPI",
+    kerberos_service_name='impala',
+    use_ssl=True,
+    use_http_transport=True,
+    http_path='cliservice',
+  )
+  
+  cursor = conn.cursor()
+  cursor.execute("select * from web_logs")
+  res = cursor.fetchall()
+  print(len(res))
+  
+  cursor.close()
+  conn.close()
+
+binary()
+hs2_http()
